a50472b2486d87d7e6c8f7fc3909e4391ddfee5e
Juan Carlos Chapoñan Sernaque <juancarlos.i18@outlook.es>
2024-09-22 07:16:00 -0500

    New animation

--- Assets/Js/Js.js
+++ Assets/Js/Js.js
@@ -1,9 +1,5 @@
 
 $(document).ready(function(){
-    CopyText();
-});
-
-function CopyText() {
     $('#btneMail').click(function() {
         navigator.clipboard.writeText("juancarlos.i18@outlook.es").then(() => {
             alert('Correo copiado al portapapeles');
@@ -11,4 +7,4 @@ function CopyText() {
             alert('Error al copiar: ', err);
         });
     });
-}
+});
