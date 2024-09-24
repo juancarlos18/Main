d2988906996e605ca18e96e2f73c1ad6b42b5c3d
Juan Carlos Chapoñan Sernaque <juancarlos.i18@outlook.es>
2024-09-23 21:39:51 -0500

    Add Light Mode

--- Assets/Js/Js.js
+++ Assets/Js/Js.js
@@ -7,6 +7,25 @@ $(document).ready(function(){
             alert('Error al copiar: ', err);
         });
     });
-});
 
-//document.addEventListener('contextmenu', event => event.preventDefault());
+    const tagA = document.getElementsByTagName('a').length;
+
+    $('#checkbox').change(function() {
+        document.body.classList.toggle("light");
+        document.getElementsByClassName('data')[0].classList.toggle("light");
+        document.getElementsByClassName('footImg')[0].classList.toggle("light");
+        document.getElementsByClassName('contBody')[0].classList.toggle("light");
+        document.getElementsByClassName('description')[0].classList.toggle("light");
+        document.getElementsByClassName('txtRow')[0].classList.toggle("light");
+
+        for (i = 0; i < tagA; i++) {
+            document.getElementsByTagName('a')[i].classList.toggle('light');
+        }
+
+        document.getElementsByTagName('button')[0].classList.toggle('light');
+        document.getElementsByClassName('txtRowSocial')[0].classList.toggle("light");
+        document.getElementsByClassName('foot')[0].classList.toggle("light");
+        document.getElementsByTagName('p')[1].classList.toggle('light');
+        document.getElementsByClassName('author')[0].classList.toggle("light");
+    });
+});
